9b74b412e7aaefd02315245c658d04e6d5b6492d
ark10806 <ark10806@gmail.com>
2021-05-25 00:23:17 +0900

    Dijkstra Completed

--- vanilia_Dijkstra.cpp
+++ vanilia_Dijkstra.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <stack>
 #define UNS 0
 #define TRE 1
 #define FRN 2
-// #define INF 1000000000
+#define END 0
 using namespace std;
-
-// (1, 2, 10)
 
 int INF = 100000000;
 
@@ -17,6 +15,7 @@ public:
     int *stat;
     int *dist;
     int *pred;
+    int *tmp_pred;
     int n;
     int s;
     queue<int> adj_que;
@@ -28,48 +27,41 @@ public:
         stat = new int[n];
         dist = new int[n];
         pred = new int[n];
+        tmp_pred = new int[n];
 
         for(int i=0; i<n; i++){
             stat[i] = UNS;    // 모든 노드를 UNSEEN으로 초기화
             dist[i] = INF;      // 모든 노드의 거리를 무한대로 초기화
-            pred[i] = -1;
+            pred[i] = END;
+            tmp_pred[i] = END;
         }
     }
     void prn(){
-        string hi;
-        getline(cin, hi);
-
-        cout << endl << "----------------------" << endl;
         for(char c='A'; c<='I'; c++){
             cout << c << '\t';
         }cout << endl;
         for(int i=0; i<n; i++){
-            if(stat[i]==UNS){
+            if(stat[i]==UNS)
                 cout << "UNS" << '\t';
-            }
-            if(stat[i]==TRE){
+            if(stat[i]==TRE)
                 cout << "TRE" << '\t';
-            }
-            if(stat[i]==FRN){
+            if(stat[i]==FRN)
                 cout << "FRN" << '\t';
-            }
         }cout << endl;
 
         for(int i=0; i<n; i++){
-            if(dist[i] == INF){
+            if(dist[i] == INF)
                 cout << "INF" << '\t';
-            }
-            else{
+            else
                 cout << dist[i] << '\t';
-            }
         }
-
-
         cout << "----------------------" << endl;
     }
     void init(){
         stat[s] = TRE;
         dist[s] = 0;
+        pred[s] = END;
+        tmp_pred[s] = END;
         getAdj(s);
         int curr;
         while(!adj_que.empty()){
@@ -78,41 +70,81 @@ public:
             pred[curr] = s;
             dist[curr] = getDist(s, curr);
         }
+        prn();
     }
     
     void calcPath(){
         init();
         while(!isComplete()){
-            prn();
+            string hi;
+            getline(cin, hi);
             int min_FRN = getMinIdx();
+            cout << endl << endl << "----------------------------" << endl;
+            cout << "min: " << (char)(min_FRN+'A') << endl;
             stat[min_FRN] = TRE;
-            // if(dist[min_FRN] > getDist(s, min_FRN)){
-            //     decreasekey(min_FRN, getDist(s, min_FRN));
+            // if(getDist(s,min_FRN) > getDist(tmp_pred[min_FRN], min_FRN)){
+            //     pred[min_FRN] = tmp_pred[min_FRN];
+            //     decreasekey(min_FRN, getDist(pred[min_FRN], min_FRN));
             // }
+            // decreasekey(min_FRN, getDist(s, min_FRN));  //B의 dist를 //A~B 2
             getAdj(min_FRN);
             int curr;
             while(!adj_que.empty()){
-                curr = adj_que.front(); adj_que.pop();
+                curr = adj_que.front(); adj_que.pop(); // curr은 min_FRN의 adjacent Nodes.
                 if(stat[curr] == UNS)
                     stat[curr] = FRN;
-                pred[curr] = min_FRN;
-                if(dist[curr] > getDist(s, curr)){
-                    decreasekey(curr, getDist(s, curr));
-                    // dist[curr] = getDist(s, curr);
+                cout << (char)(tmp_pred[curr] + 'A') << (char)(curr + 'A') << ' '  << dist[curr] << " <> " << getDist(tmp_pred[curr], curr) << endl;
+                if(dist[curr] > getDist(tmp_pred[curr], curr)){
+                    decreasekey(curr, getDist(tmp_pred[curr], curr));
+                    pred[curr] = tmp_pred[curr];
                 }
             }
+            prn();
         }
+        
         cout << endl << "RESULT#########" << endl;
         prn();
+
+        cout << "pred####"<< endl;
+        for(char c='A'; c<='I'; c++){
+            cout << c << '\t';
+        }cout << endl;
+        cout << endl;
+        for(int i=0; i<n; i++)
+            cout << (char)(pred[i] + 'A') << '\t';
+        cout << endl;
+        for(int i=0; i<n; i++)
+            cout << pred[i] << '\t';
+        cout << endl << endl;
+        for(int i=0; i<n; i++){
+            getPath(i);
+        }
+    }
+    void getPath(int node){
+        int curr = node;
+        stack<int> path;
+        cout << "PATH: A->" << (char)(node+'A') << ": "  << getDist(pred[node], node) << endl;
+        while(curr != END){
+            // cout << (char)(pred[curr] + 'A') << ' ';
+            path.push(pred[curr]);
+            curr = pred[curr];
+        }
+        while(!path.empty()){
+            if(path.top() == s)
+                cout << 'A' << "->";
+            else
+                cout << (char)(path.top()+'A') << "->";
+            path.pop();
+        }cout << (char)(node + 'A') << endl << endl;
+        // cout << pred[curr] << endl << endl;
     }
     void decreasekey(int idx, int distance){
         dist[idx] = distance;
     }
     bool isComplete(){
         for(int i=0; i<n; i++){
-            if(stat[i] == FRN){   //  || status[i] == UNS 는 없어도 되나?
+            if(stat[i] == FRN)   //  || status[i] == UNS 는 없어도 되나?
                 return false;
-            }
         }
         return true;
     }
@@ -129,26 +161,23 @@ public:
                     minIdx = i;
             }
         }
-        cout << "min: " << (char)(minIdx+'A') << endl;
         return minIdx;
     }
     void getAdj(int node){
         clearQue();
         cout << "adjs: ";
         for(int i=0; i<n; i++){
-            if(adjMat[node][i] != INF && i!=node && stat[i]==UNS){
+            if(adjMat[node][i] != INF && i!=node && stat[i]!=TRE){
                 adj_que.push(i);
+                if(node != s)
+                    tmp_pred[i] = node;
                 cout << (char)(i+'A') << '\t';
             }
         }cout << endl;
     }
     int getDist(int src, int dst){
         //src: Terminal member of TRE     dst: FRN
-        int pred_idx = pred[dst];
-        if(pred_idx == -1){
-            pred_idx = src;
-        }
-        return dist[pred_idx] + adjMat[pred_idx][dst];
+        return dist[src] + adjMat[src][dst];
     }
     void clearQue(){
         queue<int> empty;
@@ -192,6 +221,7 @@ int main(){
         }cout << endl;
     }
     cout << "cnt: " << cnt << endl;
+    cout <<"hii" << endl;
 
     Dijkstra di(adjMat, n, 0);
     di.calcPath();
